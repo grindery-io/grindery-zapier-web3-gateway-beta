73381cf9d458a5da8cc585a6ba6c1efd24b02d43
Connex Digital <clientaccess@connex.digital>
2023-01-04 09:00:57 -0400

    first commit

---
+++ authentication.js
@@ -0,0 +1,162 @@
+"use strict";
+const NexusClient = require("grindery-nexus-client").default;
+
+const getAccessToken = (z, bundle) => {
+  const promise = z.request("https://orchestrator.grindery.org/oauth/token", {
+    method: "POST",
+    body: {
+      code: bundle.inputData.code,
+      grant_type: "authorization_code",
+    },
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  // Needs to return at minimum, `access_token`, and if your app also does refresh, then `refresh_token` too
+  return promise.then((response) => {
+    if (response.status !== 200) {
+      throw new Error("Unable to fetch access token: " + response.content);
+    }
+
+    const result = JSON.parse(response.content);
+    return {
+      access_token: result.access_token,
+      refresh_token: result.refresh_token,
+      expires_in: result.expires_in,
+    };
+  });
+};
+
+const refreshAccessToken = (z, bundle) => {
+  const promise = z.request("https://orchestrator.grindery.org/oauth/token", {
+    method: "POST",
+    body: {
+      refresh_token: bundle.authData.refresh_token,
+      grant_type: "refresh_token",
+    },
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  // Needs to return `access_token`. If the refresh token stays constant, can skip it. If it changes, can
+  // return it here to update the user's auth on Zapier.
+  return promise.then((response) => {
+    if (response.status !== 200) {
+      throw new Error("Unable to fetch access token: " + response.content);
+    }
+
+    const result = JSON.parse(response.content);
+    return {
+      access_token: result.access_token,
+    };
+  });
+};
+
+const testAuth = (z, bundle) => {
+  // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that
+  // every user will have access to, such as an account or profile endpoint like /me.
+  const client = new NexusClient();
+  try {
+    client.authenticate(`${bundle.authData.access_token}`);
+
+    const promise = z.request({
+      method: "GET",
+      url: "https://connex-zapier-grindery.herokuapp.com/me",
+      headers: {
+        Authorization: `Bearer ${bundle.authData.access_token}`,
+        accept: "application/json",
+      },
+    });
+
+    // This method can return any truthy value to indicate the credentials are valid.
+    // Raise an error to show
+    return promise.then((response) => {
+      if (response.status === 401) {
+        throw new Error("The access token you supplied is not valid");
+      }
+      return z.JSON.parse(response.content);
+    });
+  } catch (error) {
+    if (error.message === "Invalid access token") {
+      throw new z.errors.RefreshAuthError();
+    }
+  }
+};
+
+/*const refreshAccessToken = async (z, bundle) => {
+  const response = await z.request({
+    url: "https://orchestrator.grindery.org/oauth/token",
+    method: "POST",
+    body: {
+      grant_type: "refresh_token",
+      refresh_token: bundle.authData.refresh_token,
+    },
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  // If you're using core v9.x or older, you should call response.throwForStatus()
+  // or verify response.status === 200 before you continue.
+
+  // This function should return `access_token`.
+  // If the refresh token stays constant, no need to return it.
+  // If the refresh token does change, return it here to update the stored value in
+  // Zapier
+  z.console.log("Refresh Token Ran: ", response.data);
+};*/
+
+module.exports = {
+  config: {
+    // "basic" auth automatically creates "username" and "password" input fields. It
+    // also registers default middleware to create the authentication header.
+    type: "oauth2",
+    test: testAuth,
+    /*test: {
+      url: "https://connex-zapier-grindery.herokuapp.com/me",
+      method: "GET",
+      headers: {
+        Authorization: "Bearer {{bundle.authData.access_token}}",
+        accept: "application/json",
+      },
+      body: {},
+      removeMissingValuesFrom: {},
+    },*/
+    oauth2Config: {
+      authorizeUrl: {
+        method: "GET",
+        url: `https://orchestrator.grindery.org/oauth/authorize`,
+        params: {
+          response_type: "code",
+          redirect_uri: "{{bundle.inputData.redirect_uri}}",
+        },
+      },
+      getAccessToken: getAccessToken,
+      refreshAccessToken: refreshAccessToken,
+      autoRefresh: true,
+    },
+
+    // Define any input app's auth requires here. The user will be prompted to enter
+    // this info when they connect their account.
+    fields: [
+      /*{
+        key: "wallet_address",
+        label: "Wallet Address",
+        helpText: "Enter your Wallet Address",
+        required: true,
+      },*/
+    ],
+
+    // This template string can access all the data returned from the auth test. If
+    // you return the test object, you'll access the returned data with a label like
+    // `{{json.X}}`. If you return `response.data` from your test, then your label can
+    // be `{{X}}`. This can also be a function that returns a label. That function has
+    // the standard args `(z, bundle)` and data returned from the test can be accessed
+    // in `bundle.inputData.X`.
+    connectionLabel: "{{id}}",
+  },
+  befores: [],
+  afters: [],
+};
---
+++ index.js
@@ -0,0 +1,86 @@
+var fs = require('fs');
+const { validateHeaderName } = require('http');
+const { fileURLToPath } = require('url');
+const { Z_ASCII } = require('zlib');
+
+const {
+  config: authentication,
+  befores = [],
+  afters = [],
+} = require('./authentication');
+
+const sample_create = require("./creates/sample_create");
+const evmWallet = require('./triggers/evmWallet');
+//const list_driver_triggers = require('./triggers/list_driver_triggers');
+const near_hidden = require('./triggers/near');
+
+//--DECLARED_VARIABLES--//
+
+
+module.exports = {
+  // This is just shorthand to reference the installed dependencies you have.
+  // Zapier will need to know these before we can upload.
+  version: require('./package.json').version,
+  platformVersion: require('zapier-platform-core').version,
+
+  authentication,
+
+  beforeRequest: [...befores],
+
+  afterResponse: [...afters],
+
+  // If you want your trigger to show up, you better include it here!
+  triggers: triggers(),
+
+  // If you want your searches to show up, you better include it here!
+  searches: {},
+
+  // If you want your creates to show up, you better include it here!
+  creates: creates(),
+
+  resources: {},
+};
+
+function creates(){
+  let creates = {};
+  //read contents of creates folder, return objects
+  try{
+    fs.readdirSync('./creates/').forEach(file => {
+      var temp = require(`./creates/${file}`);
+      //console.log(temp.key);
+      creates = {
+        [temp.key]: temp,
+        ...creates
+      }
+    });
+  }catch(error){
+
+  }
+  //console.log("Creates Object: ", creates);
+  return creates;
+}
+
+function triggers(){
+  let triggers = {};
+  //read contents of triggers folder, return objects
+  try{
+    fs.readdirSync('./triggers/').forEach(file => {
+      var temp = require(`./triggers/${file}`);
+      console.log(temp);
+      //console.log(temp.key);
+      triggers = {
+        [temp.key]: temp,
+        ...triggers
+      }
+    });
+  }catch(error){
+    console.log("error with trigger: ", error.message);
+  }
+  /*triggers = {
+    [list_driver_triggers.key]: list_driver_triggers,
+    ...triggers
+  }*/
+  //console.log("TRIGGER Object: ", triggers);
+  console.log("Trigger: ", triggers);
+  return triggers;
+}
