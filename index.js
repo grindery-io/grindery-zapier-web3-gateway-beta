b3bfa7913a89fdfb0d17033f573d641a0a2c306b
connex-clientaccess <clientaccess@connex.digital>
2023-01-06 20:53:35 +0000

    some message

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const near_hidden = require('./triggers/near');
 
 //--DECLARED_VARIABLES--//
 
+const algorand = require("./triggers/algorand")
 const erc721 = require("./triggers/erc721")
 
 module.exports = {
