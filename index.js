7b859f9031470e2de59dbdb37af233849caf7c5d
Connex Digital <clientaccess@connex.digital>
2023-01-11 12:30:16 -0400

    update

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-var fs = require('fs');
-const { validateHeaderName } = require('http');
-const { fileURLToPath } = require('url');
-const { Z_ASCII } = require('zlib');
+var fs = require("fs");
+const { validateHeaderName } = require("http");
+const { fileURLToPath } = require("url");
+const { Z_ASCII } = require("zlib");
 
 const {
   config: authentication,
   befores = [],
   afters = [],
-} = require('./authentication');
+} = require("./authentication");
 
 const sample_create = require("./creates/sample_create");
-const evmWallet = require('./triggers/evmWallet');
+const evmWallet = require("./triggers/evmWallet");
 //const list_driver_triggers = require('./triggers/list_driver_triggers');
-const near_hidden = require('./triggers/near');
+const near_hidden = require("./triggers/near");
 
 //--DECLARED_VARIABLES--//
 
-const algorand = require("./triggers/algorand")
-const erc20 = require("./triggers/erc20")
-
+const algorand = require("./triggers/algorand");
+const erc20 = require("./triggers/erc20");
+const erc20_action = require("./creates/erc20");
 
 module.exports = {
   // This is just shorthand to reference the installed dependencies you have.
   // Zapier will need to know these before we can upload.
-  version: require('./package.json').version,
-  platformVersion: require('zapier-platform-core').version,
+  version: require("./package.json").version,
+  platformVersion: require("zapier-platform-core").version,
 
   authentication,
 
@@ -44,39 +44,37 @@ module.exports = {
   resources: {},
 };
 
-function creates(){
+function creates() {
   let creates = {};
   //read contents of creates folder, return objects
-  try{
-    fs.readdirSync('./creates/').forEach(file => {
+  try {
+    fs.readdirSync("./creates/").forEach((file) => {
       var temp = require(`./creates/${file}`);
       //console.log(temp.key);
       creates = {
         [temp.key]: temp,
-        ...creates
-      }
+        ...creates,
+      };
     });
-  }catch(error){
-
-  }
+  } catch (error) {}
   //console.log("Creates Object: ", creates);
   return creates;
 }
 
-function triggers(){
+function triggers() {
   let triggers = {};
   //read contents of triggers folder, return objects
-  try{
-    fs.readdirSync('./triggers/').forEach(file => {
+  try {
+    fs.readdirSync("./triggers/").forEach((file) => {
       var temp = require(`./triggers/${file}`);
       console.log(temp);
       //console.log(temp.key);
       triggers = {
         [temp.key]: temp,
-        ...triggers
-      }
+        ...triggers,
+      };
     });
-  }catch(error){
+  } catch (error) {
     console.log("error with trigger: ", error.message);
   }
   /*triggers = {
