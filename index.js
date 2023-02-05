8358fb247396c4720891dc9339c36d19561c2381
connex-clientaccess <clientaccess@connex.digital>
2023-02-05 18:09:43 -0300

    push

--- index.js
+++ index.js
@@ -44,8 +44,6 @@ const erc721 = require("./triggers/erc721")
 
 
 
-
-const astroDao = require("./triggers/astroDao")
 const evmWallet_action = require("./creates/evmWallet")
 const evmWallet = require("./triggers/evmWallet")
 
